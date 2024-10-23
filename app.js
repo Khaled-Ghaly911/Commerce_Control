6f819191e51b448cecc307b26fff8c9bbc5b894d
Khaled_Ghaly <khaledghaly000@gmail.com>
2024-10-23 14:57:32 +0300

    modify port number & css

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const sequelize = require('./util/database') 
+const sequelize = require('./util/database')
 
 const app = express();
 
@@ -24,7 +24,7 @@ app.use(errorController.get404);
 
 sequelize.sync().then(result => {
     console.log(result);
-    app.listen(3000);
+    app.listen(4000);
 }).catch(err => {
     console.log(err);
 })
