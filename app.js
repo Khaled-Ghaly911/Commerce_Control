52bfd9a35b8a195b15298279f92910a46107db0b
Khaled_Ghaly <khaledghaly000@gmail.com>
2024-11-05 15:20:41 +0200

    feature adding authentication

--- app.js
+++ app.js
@@ -5,17 +5,21 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const MONGODB_URI = 'mongodb+srv://khaledghaly000:D0sQDtwUBCqoBhc6@cluster0.s5xgw.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0';
+const MONGODB_URI =
+  'mongodb+srv://khaledghaly000:NhTAOKbbUFlnCJsS@cluster0.s5xgw.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0';
 
 const app = express();
 const store = new MongoDBStore({
-    uri: MONGODB_URI,
-    collection: 'sessions'
+  uri: MONGODB_URI,
+  collection: 'sessions'
 });
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -26,25 +30,33 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false, store: store }));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
+app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
-    if(!req.session.user) {
-        return next();
-    }
-    User.findById(req.session.user._id).then(user => {
-        req.user = user;
-        next();
-    }).catch(err => console.log(err))
-})
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use((req, res, next) => {
-    User.findById('6721e90d96211c68b7e02e6a')
-        .then(user => {
-            req.user = user;
-            next();
-        })
-        .catch(err => console.log(err));
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
 });
 
 app.use('/admin', adminRoutes);
@@ -54,24 +66,10 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-    .connect(
-        MONGODB_URI
-    )
-    .then(result => {
-        User.findOne().then(user => {
-            if (!user) {
-                const user = new User({
-                    name: 'khaled',
-                    email: 'khaled@gmail.com',
-                    cart: {
-                        items: []
-                    }
-                });
-                user.save();
-            }
-        });
-        app.listen(4000);
-    })
-    .catch(err => {
-        console.log(err);
-    });
+  .connect(MONGODB_URI)
+  .then(result => {
+    app.listen(4000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
